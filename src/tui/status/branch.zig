e7f2bd420639bfa67e35239fc1cc2a8f75df7e8b
Igor Támara <igor.tamara@gmail.com>
2025-10-25 08:32:41 -0500

    feat: update vcs changed files info when click on branch status

--- src/tui/status/branch.zig
+++ src/tui/status/branch.zig
@@ -64,6 +64,7 @@ pub fn ctx_deinit(self: *Self) void {
 
 fn on_click(self: *Self, _: *ButtonType, _: Widget.Pos) void {
     self.refresh_git_status();
+    tui.sync_with_vcs() catch {};
     command.executeName("show_git_status", .{}) catch {};
 }
 
